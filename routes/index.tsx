e08671252a5f6b0f2f9a0277b37eacd89f4e1357
Hexagon <hexagon@56k.guru>
2023-01-10 22:04:15 +0100

    en does not have a default page

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,7 +2,9 @@ import { Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = {
   GET(_req, ctx) {
-    // Redirect the user to the default country of their language
+    // Redirect the user to the default country of their language, do not allow en as it does not
+    // have it's own page
+    if (ctx.state.lang === "en") ctx.state.lang = "sv";
     return new Response("", {
       status: 307,
       headers: { Location: "/" + ctx.state.lang },
