49933e8a85619b9674b39219e1951cf1c3e3b728
Hexagon <hexagon@56k.guru>
2023-01-15 20:55:26 +0100

    Add price update schedule at 8,9,10 am

--- backend/scheduler/index.ts
+++ backend/scheduler/index.ts
@@ -12,6 +12,7 @@ const jobs = [
   new Cron("0 45,50,54,58 12 * * *", { paused: true, timezone: "Europe/Oslo" }, DailyPriceUpdate),
   new Cron("0 0,2,4,7,11,15,20,27,35,45,55 13 * * *", { paused: true, timezone: "Europe/Oslo" }, DailyPriceUpdate),
   new Cron("0 5,20,40,58 14 * * *", { paused: true, timezone: "Europe/Oslo" }, DailyPriceUpdate),
+  new Cron("0 4 8,9,10 * * *", { paused: true, timezone: "Europe/Oslo" }, DailyPriceUpdate),
   new Cron("0 14 * * * *", { paused: true, timezone: "Europe/Oslo" }, DailyCurrencyUpdate),
   new Cron("0 2,32 * * * *", { paused: true, timezone: "Europe/Oslo" }, HourlyConsumptionUpdate),
   new Cron("0 4,34 * * * *", { paused: true, timezone: "Europe/Oslo" }, HourlyProductionUpdate),
